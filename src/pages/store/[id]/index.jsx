eaf30861e45b6f7657afc4db0d07b4fd06ec4555
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-03 21:42:50 +0530

    added tabs inthe store

--- src/pages/store/[id]/index.jsx
+++ src/pages/store/[id]/index.jsx
@@ -1,11 +1,37 @@
-import StoreLayout from "@/components/sellerStore/storeLayout";
 import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import RecentOrdersCard from "@/components/sellerStore/recentOrdersCard";
+import TopSellingProductsCard from "@/components/sellerStore/topSellingProductsCard";
+import NewCustomersCard from "@/components/sellerStore/newCustomersCard";
+import AnalyticsCard from "@/components/sellerStore/analyticsCard";
 
 const StoreHome = () => {
   return (
     <div>
-      <StoreLayout />
+      <Tabs defaultValue="orders">
+        <TabsList className="w-max">
+          <TabsTrigger value="orders">ORDERS</TabsTrigger>
+          <TabsTrigger value="products">PRODUCTS</TabsTrigger>
+          <TabsTrigger value="customers">CUSTOMERS</TabsTrigger>
+          <TabsTrigger value="analytics">ANALYTICS</TabsTrigger>
+          <TabsTrigger value="settings">SETTINGS</TabsTrigger>
+        </TabsList>
+        <TabsContent value="orders">
+          <RecentOrdersCard />
+        </TabsContent>
+        <TabsContent value="products">
+          <TopSellingProductsCard />
+        </TabsContent>
+        <TabsContent value="customers">
+          <NewCustomersCard />
+        </TabsContent>
+        <TabsContent value="analytics">
+          <AnalyticsCard />
+        </TabsContent>
+        <TabsContent value="settings">SETTINGS CARD</TabsContent>
+      </Tabs>
     </div>
   );
 };
+
 export default StoreHome;
