10a9cb05f135668d6adbdaa9a7492524a072a7ee
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-03 18:18:24 +0530

    added addProduct card ui

---
+++ src/components/product/addProduct.jsx
@@ -0,0 +1,202 @@
+import React, { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { ToggleGroup, ToggleGroupItem } from "../ui/toggle-group";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import Image from "next/image";
+import { X } from "lucide-react";
+import { Button } from "../ui/button";
+
+const AddProduct = () => {
+  const [addProduct, setAddProduct] = useState({
+    productName: "",
+    description: "",
+    categories: "",
+    size: "",
+    price: "",
+    offerPrice: "",
+    stockLevel: "",
+    images: "",
+    sku: "",
+    colors: "",
+    userId: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setAddProduct((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  const handleSingleSelection = (field, value) => {
+    setAddProduct((prevState) => ({
+      ...prevState,
+      [field]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(addProduct);
+  };
+
+  return (
+    <div>
+      <h1 className="text-center font-bold text-xl">ADD NEW PRODUCT</h1>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div className="space-y-2">
+          <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
+            <div className="space-y-2">
+              <Label htmlFor="productName">Product Name</Label>
+              <Input
+                id="productName"
+                value={addProduct.productName}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="sku">SKU</Label>
+              <Input
+                id="sku"
+                value={addProduct.sku}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="description">Product Description</Label>
+            <Textarea
+              id="description"
+              value={addProduct.description}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="grid grid-cols-1 items-center md:grid-cols-2 gap-2">
+            <div>
+              <Label htmlFor="size">Sizes</Label>
+              <ToggleGroup
+                type="single"
+                className="flex gap-2"
+                value={addProduct.size}
+                onValueChange={(value) => handleSingleSelection("size", value)}
+              >
+                <ToggleGroupItem value="xs">XS</ToggleGroupItem>
+                <ToggleGroupItem value="s">S</ToggleGroupItem>
+                <ToggleGroupItem value="m">M</ToggleGroupItem>
+                <ToggleGroupItem value="l">L</ToggleGroupItem>
+                <ToggleGroupItem value="xl">XL</ToggleGroupItem>
+              </ToggleGroup>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="colors">Colors</Label>
+              <ToggleGroup
+                type="single"
+                className="flex gap-2"
+                value={addProduct.colors}
+                onValueChange={(value) =>
+                  handleSingleSelection("colors", value)
+                }
+              >
+                <ToggleGroupItem value="black">Black</ToggleGroupItem>
+                <ToggleGroupItem value="white">White</ToggleGroupItem>
+                <ToggleGroupItem value="red">Red</ToggleGroupItem>
+                <ToggleGroupItem value="blue">Blue</ToggleGroupItem>
+                <ToggleGroupItem value="green">Green</ToggleGroupItem>
+              </ToggleGroup>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <div className="space-y-2">
+              <Label htmlFor="categories">Product Category</Label>
+              <Select
+                id="categories"
+                onValueChange={(value) =>
+                  setAddProduct((prevState) => ({
+                    ...prevState,
+                    categories: value,
+                  }))
+                }
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select a category" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="t-shirt">T-SHIRT</SelectItem>
+                  <SelectItem value="shirt">SHIRT</SelectItem>
+                  <SelectItem value="pant">PANT</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="stockLevel">Stock Levels</Label>
+              <Input
+                id="stockLevel"
+                value={addProduct.stockLevel}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <div className="space-y-2">
+              <Label htmlFor="price">Price</Label>
+              <Input
+                id="price"
+                value={addProduct.price}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="offerPrice">Offer Price</Label>
+              <Input
+                id="offerPrice"
+                value={addProduct.offerPrice}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 items-center md:grid-cols-2 gap-2">
+            <div className="space-y-2">
+              <Label htmlFor="images">Images</Label>
+              <Input
+                id="images"
+                value={addProduct.images}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="flex gap-2">
+              <div className="relative">
+                <Image
+                  src="https://d30b9hrf6faw09.cloudfront.net/upload/20240808200545WhatsApp%20Image%202024-08-08%20at%208.04.53%20PM.jpeg"
+                  width={50}
+                  height={50}
+                  alt=""
+                  className="rounded-sm"
+                />
+                <X
+                  size={10}
+                  className="absolute cursor-pointer -top-1 -right-1 h-5 w-5 bg-red-500 rounded-full text-white"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full">
+          <Button className="w-full" type="submit">
+            UPLOAD PRODUCT
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddProduct;
--- src/components/sellerStore/topSellingProductsCard.jsx
+++ src/components/sellerStore/topSellingProductsCard.jsx
@@ -8,29 +8,51 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+import AddProduct from "../product/addProduct";
+import { EllipsisVerticalIcon } from "lucide-react";
 
 const TopSellingProductsCard = () => {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Top SellingProducts</CardTitle>
+        <CardTitle className="flex justify-between items-center">
+          <h1>PRODUCT DETAILS</h1>
+          <Dialog>
+            <DialogTrigger>
+              <Button>Add Product</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <AddProduct />
+            </DialogContent>
+          </Dialog>
+        </CardTitle>
       </CardHeader>
       <CardContent>
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead className="w-[150px]">Product</TableHead>
-              <TableHead className="w-[150px]">Price</TableHead>
-              <TableHead className="w-[150px]">Quantity</TableHead>
-              <TableHead className="w-[150px]">Revenue</TableHead>
+              <TableHead className="w-[150px]">PRODUCT NAME</TableHead>
+              <TableHead className="w-[50px]">CATEGORY</TableHead>
+              <TableHead className="w-[50px]">PRICE</TableHead>
+              <TableHead className="w-[50px]">STOCKS</TableHead>
+              <TableHead className="w-[100px]">SIZES</TableHead>
+              <TableHead className="w-[100px]">COLORS</TableHead>
+              <TableHead className="w-[50px]">ACTIONS</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             <TableRow>
               <TableCell className="font-medium">Casuual T-Shirt</TableCell>
-              <TableCell>$29.99</TableCell>
-              <TableCell>500</TableCell>
-              <TableCell>$250.00</TableCell>
+              <TableCell>T-SHIRT</TableCell>
+              <TableCell>$50.0</TableCell>
+              <TableCell>250</TableCell>
+              <TableCell>X</TableCell>
+              <TableCell>Black</TableCell>
+              <TableCell>
+                <EllipsisVerticalIcon />
+              </TableCell>
             </TableRow>
           </TableBody>
         </Table>
