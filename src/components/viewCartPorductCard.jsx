608e3036add4331d770ff80cc0caa0fda926a61a
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-13 11:11:22 +0530

    added skeleton loading for cart and wishlist

--- src/components/viewCartPorductCard.jsx
+++ src/components/viewCartPorductCard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Card, CardContent } from "./ui/card";
+import { Card, CardContent, CardFooter } from "./ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import { Badge } from "./ui/badge";
@@ -13,11 +13,12 @@ import {
   cartSuccess,
 } from "@/store/actions/cart.slice";
 import axios from "axios";
+import { Skeleton } from "./ui/skeleton";
 
 const ViewCartProductCard = () => {
   const { user } = useSelector((state) => state.auth);
 
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, loading } = useSelector((state) => state.cart);
 
   const { API_URI } = ENV_VAR;
   const TOKEN =
@@ -78,73 +79,93 @@ const ViewCartProductCard = () => {
 
   return (
     <>
-      {cartItems.map((cart, index) => (
-        <Card key={index} className="h-max w-full sm:h-[200px] relative">
-          <Badge
-            onClick={() => handleRemoveFromCart(cart.product._id)}
-            className="absolute cursor-pointer -right-2  -top-2 p-1 bg-gray-300 text-gray-500 hover:bg-gray-200 ">
-            <X size={12} />
-          </Badge>
-          <CardContent key={index} className="h-full p-2  ">
-            <div className="flex gap-2 items-start justify-start">
-              <div className="h-[180px]">
-                <Link href={`/products/${cart.product._id}`}>
-                  <Image
-                    width={150}
-                    height={500}
-                    src={cart.product.images[0]}
-                    alt=""
-                    className="h-full object-contain rounded-xl"
-                  />
-                </Link>
-              </div>
-              <div>
-                <div className="flex flex-col gap-2">
-                  <h1 className="text-xl font-semibold">
-                    {cart.product.productName}
-                  </h1>
-                  <div className="flex items-center gap-1">
-                    <span>Size : </span>
-                    <span className="h-7 w-7 border bg-red-500 text-white transition-all duration-300 rounded-sm flex justify-center items-center uppercase ">
-                      {cart.size}
-                    </span>
+      {loading
+        ? Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <Card key={index} className="h-max w-full sm:h-[200px] relative">
+                <CardContent>
+                  <div className="flex gap-2 items-start justify-start">
+                    <div className="h-[180px] w-50%">
+                      <Skeleton className="h-[200px] w-[150px] rounded-sm" />
+                    </div>
+                    <div>
+                      <Skeleton className="w-[100px] h-[20px] rounded-full" />
+                      <Skeleton className="w-[150px] h-[15px] mt-2 rounded-full" />
+                      <Skeleton className="w-[80px] h-[15px] mt-2 rounded-full" />
+                    </div>
                   </div>
-                  <p className="flex items-center gap-2">
-                    Color :{" "}
-                    <p
-                      style={{
-                        backgroundColor: cart.color,
-                        color: getContrastColor(cart.color),
-                      }}
-                      className="h-5 w-5 bg-red-500 rounded-full"
-                    />
-                  </p>
-                  <div className="flex flex-wrap gap-4 items-center">
-                    <small>
-                      Fabric :{" "}
-                      <span className="font-semibold">
-                        {cart.product.fabric}
-                      </span>
-                    </small>
-                    <small>
-                      Quantity :{" "}
-                      <span className="font-semibold">{cart.quantity}</span>
-                    </small>
+                </CardContent>
+              </Card>
+            ))
+        : cartItems.map((cart, index) => (
+            <Card key={index} className="h-max w-full sm:h-[200px] relative">
+              <Badge
+                onClick={() => handleRemoveFromCart(cart.product._id)}
+                className="absolute cursor-pointer -right-2  -top-2 p-1 bg-gray-300 text-gray-500 hover:bg-gray-200 "
+              >
+                <X size={12} />
+              </Badge>
+              <CardContent key={index} className="h-full p-2  ">
+                <div className="flex gap-2 items-start justify-start">
+                  <div className="h-[180px]">
+                    <Link href={`/products/${cart.product._id}`}>
+                      <Image
+                        width={150}
+                        height={500}
+                        src={cart.product.images[0]}
+                        alt=""
+                        className="h-full object-contain rounded-xl"
+                      />
+                    </Link>
                   </div>
-                  <div className="font-mono flex gap-2 items-center">
-                    <span className="text-2xl font-semibold">
-                      ₹{cart.product.price}
-                    </span>
-                    <span className="line-through text-gray-500">
-                      ₹{cart.product.offerPrice}
-                    </span>
+                  <div>
+                    <div className="flex flex-col gap-2">
+                      <h1 className="text-xl font-semibold">
+                        {cart.product.productName}
+                      </h1>
+                      <div className="flex items-center gap-1">
+                        <span>Size : </span>
+                        <span className="h-7 w-7 border bg-red-500 text-white transition-all duration-300 rounded-sm flex justify-center items-center uppercase ">
+                          {cart.size}
+                        </span>
+                      </div>
+                      <p className="flex items-center gap-2">
+                        Color :{" "}
+                        <p
+                          style={{
+                            backgroundColor: cart.color,
+                            color: getContrastColor(cart.color),
+                          }}
+                          className="h-5 w-5 bg-red-500 rounded-full"
+                        />
+                      </p>
+                      <div className="flex flex-wrap gap-4 items-center">
+                        <small>
+                          Fabric :{" "}
+                          <span className="font-semibold">
+                            {cart.product.fabric}
+                          </span>
+                        </small>
+                        <small>
+                          Quantity :{" "}
+                          <span className="font-semibold">{cart.quantity}</span>
+                        </small>
+                      </div>
+                      <div className="font-mono flex gap-2 items-center">
+                        <span className="text-2xl font-semibold">
+                          ₹{cart.product.price}
+                        </span>
+                        <span className="line-through text-gray-500">
+                          ₹{cart.product.offerPrice}
+                        </span>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
-      ))}
+              </CardContent>
+            </Card>
+          ))}
     </>
   );
 };
--- src/components/wishListCard.jsx
+++ src/components/wishListCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Card, CardContent, CardFooter } from "./ui/card";
 import { Badge } from "./ui/badge";
 import Link from "next/link";
@@ -13,7 +13,7 @@ import {
   wishListSuccess,
   wishListData,
 } from "@/store/actions/wishList.slice";
-
+import { Skeleton } from "./ui/skeleton";
 const saleTypeColors = {
   "HOT SALE": "bg-red-500",
   "NEW ARRIVALS": "bg-blue-500",
@@ -23,7 +23,7 @@ const saleTypeColors = {
 };
 
 const WishListCard = () => {
-  const { wishListItems } = useSelector((state) => state.wishList);
+  const { wishListItems, loading } = useSelector((state) => state.wishList);
   const { toast } = useToast();
   const { user } = useSelector((state) => state.auth);
   const { API_URI } = ENV_VAR;
@@ -110,17 +110,35 @@ const WishListCard = () => {
 
   return (
     <>
-      {wishListItems.length === 0
+      {loading
+        ? Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <Card
+                key={index}
+                className="w-full overflow-hidden h-full bg-white transition-all duration-700 relative"
+              >
+                <Skeleton className="w-[300px] h-[300px] rounded-sm" />
+                <CardFooter className="p-2 my-2 h-[30%] w-full flex justify-center flex-col items-start">
+                  <Skeleton className="w-[100px] h-[20px] rounded-full" />
+                  <Skeleton className="w-[150px] h-[15px] mt-2 rounded-full" />
+                  <Skeleton className="w-[80px] h-[15px] mt-2 rounded-full" />
+                </CardFooter>
+              </Card>
+            ))
+        : wishListItems.length === 0
         ? "NO ITEMS IN WIHS LIST"
         : wishListItems.map((wishListItem, index) => (
             <Card
               key={index}
-              className="w-full overflow-hidden h-full bg-white hover:shadow-xl transition-all duration-700 relative hover:-translate-y-2 ease-in-out">
+              className="w-full overflow-hidden h-full bg-white hover:shadow-xl transition-all duration-700 relative hover:-translate-y-2 ease-in-out"
+            >
               {wishListItem.saleType && (
                 <Badge
                   className={`${
                     saleTypeColors[wishListItem.saleType]
-                  } rounded-l absolute top-0 left-0 tracking-widest`}>
+                  } rounded-l absolute top-0 left-0 tracking-widest`}
+                >
                   {wishListItem.saleType}
                 </Badge>
               )}
@@ -132,7 +150,8 @@ const WishListCard = () => {
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="currentColor"
-                    className="size-5 text-red-500 cursor-pointer">
+                    className="size-5 text-red-500 cursor-pointer"
+                  >
                     <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                   </svg>
                 ) : (
@@ -143,7 +162,8 @@ const WishListCard = () => {
                     viewBox="0 0 24 24"
                     strokeWidth={1.5}
                     stroke="currentColor"
-                    className="size-5 text-white cursor-pointer">
+                    className="size-5 text-white cursor-pointer"
+                  >
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
@@ -166,7 +186,8 @@ const WishListCard = () => {
               <CardFooter className="p-2 my-2  h-[30%]  w-full flex justify-center flex-col items-start">
                 <Link
                   href={`/products/${wishListItem._id}`}
-                  className="transition-all duration-200 hover:text-red-500">
+                  className="transition-all duration-200 hover:text-red-500"
+                >
                   <h1 className="text-md font-medium">
                     {wishListItem.productName}
                   </h1>
