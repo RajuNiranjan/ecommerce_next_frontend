e6318ad7831379d9454b4fa576fc12c96b606181
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-29 13:36:18 +0530

    fixed skeliton loading for product card

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,9 @@ import {
 } from "@/store/actions/product.slice";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { Skeleton } from "@/components/ui/skeleton"
+import { Skeleton } from "@/components/ui/skeleton";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { addressStart } from "@/store/actions/address.slice";
-
-
 
 const Home = () => {
   const { API_URI } = ENV_VAR;
@@ -35,35 +33,35 @@ const Home = () => {
     fetchProducts();
   }, [API_URI, dispatch]);
 
-
-
   return (
     <>
       <div className="p-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6  gap-4">
-        {loading ? (
-          Array(12)
-            .fill(0)
-            .map((_, index) => (
-              <Card key={index} className="w-full h-[300px]">
-                <CardContent className="p-0 h-[70%] bg-slate-200">
-                  <Skeleton className="h-full w-full" />
-                </CardContent>
-                <CardFooter className="p-2 flex flex-col gap-4  my-2 h-[30%]">
-                  <Skeleton className="h-3 rounded-full w-full" />
-                  <div className="flex gap-4 justify-between items-center">
-                    <Skeleton className="h-3 w-32  rounded-full " />
-                    <Skeleton className="h-3 w-8  rounded-full " />
-                  </div>
-                  <div className="flex gap-4 justify-between items-center">
-                    <Skeleton className="h-3 w-32  rounded-full " />
-                    <Skeleton className="h-3 w-8  rounded-full " />
-                  </div>
-                </CardFooter>
-              </Card>
-            ))
-        ) : allProducts.map((product) => (
-          <ProductCard key={product._id} product={product} />
-        ))}
+        {loading
+          ? Array(12)
+              .fill(0)
+              .map((_, index) => (
+                <Card
+                  key={index}
+                  className="w-full h-full max-h-[450px] min-h-[300px]">
+                  <CardContent className="p-0 h-[70%] bg-slate-200">
+                    <Skeleton className="h-full w-full" />
+                  </CardContent>
+                  <CardFooter className="p-2 flex flex-col gap-4  my-2 h-[30%]">
+                    <Skeleton className="h-3 rounded-full w-full" />
+                    <div className="flex gap-4 w-full justify-between items-center">
+                      <Skeleton className="h-3 w-[80%]  rounded-full " />
+                      <Skeleton className="h-3 w-[20%]  rounded-full " />
+                    </div>
+                    <div className="flex gap-4 w-full justify-between items-center">
+                      <Skeleton className="h-3 w-[80%]  rounded-full " />
+                      <Skeleton className="h-3 w-[20%]  rounded-full " />
+                    </div>
+                  </CardFooter>
+                </Card>
+              ))
+          : allProducts.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
       </div>
     </>
   );
